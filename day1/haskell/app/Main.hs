2ab32cfba2ca1591c06d14127e4a68be7b16c4c3
v1p3rh0rr0r <v1p3rh0rr0r@protonmail.com>
2021-12-02 20:40:35 +0100

    day1 puzzle1 haskell

--- day1/haskell/app/Main.hs
+++ day1/haskell/app/Main.hs
@@ -3,7 +3,21 @@ module Main where
 main :: IO ()
 main = do
     s <- readFile "/home/larodar/source/AdventOfCode/day1/input.txt"
-    (print . toInt . lines) s
+    let list = (toInt . lines) s
+    (print . tupleFirst . compareWithNext) (0, 0, list)
+
+tupleFirst :: (a, b) -> a
+tupleFirst (x, _) = x
 
 toInt :: [String] -> [Int]
 toInt = map read
+
+compareWithNext :: (Int, Int, [Int]) -> (Int, [Int])
+compareWithNext (0, 0, list) = compareWithNext (0, head list, tail list)
+compareWithNext (count, _, []) = (count, [])
+compareWithNext (count, last, list) = do
+    let current = head list
+    if last < current then 
+      compareWithNext (count + 1, head list, tail list) 
+    else 
+      compareWithNext(count, current, tail list)
