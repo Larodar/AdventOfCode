cc330c5ed2d41e9b84eb8f9e0d48247732233de4
v1p3rh0rr0r <v1p3rh0rr0r@protonmail.com>
2023-11-30 21:43:19 +0100

    add session view and request, add active session marker to sessionstate

--- aoc2022/day7/src/main.rs
+++ aoc2022/day7/src/main.rs
@@ -1,7 +1,4 @@
-use std::{
-    io::{stdin, Read},
-    str::Lines,
-};
+use std::io::{stdin, Read};
 
 fn main() {
     let mut buf = String::new();
@@ -14,14 +11,75 @@ fn main() {
         .map(|s| s.parse::<u32>().unwrap())
     {
         Some(1) => println!("{}", p1(&buf[..])),
-        //Some(2) => println!("{}", p2(&buf[..])),
+        Some(2) => println!("{}", p2(&buf[..])),
         _ => panic!("specify the part of the solution to run (1/2)"),
     }
 }
 
+fn p2(input: &str) -> u64 {
+    const FS_SIZE: u64 = 70000000;
+    const MIN_REQUIRED: u64 = 30000000;
+    let fs = FsEntry::from_lines(&mut input.lines());
+
+    let mut used = fs.total_size() as u64;
+    let to_free = MIN_REQUIRED - (FS_SIZE - used);
+    if fs.kids.is_some() {
+        walk_p2(&fs, &mut used, to_free);
+        used
+    } else if fs.size < 100_000 {
+        fs.size as u64
+    } else {
+        panic!("This case was not expected.")
+    }
+}
+
+fn walk_p2(fs: &FsEntry, free_size: &mut u64, required: u64) {
+    assert_eq!(fs.size, 0);
+    for dir in fs
+        .kids
+        .as_ref()
+        .unwrap()
+        .iter()
+        .filter(|d| d.kids.is_some())
+    {
+        let dir_total = dir.total_size() as u64;
+        if dir_total > required {
+            *free_size = std::cmp::min(*free_size, dir_total);
+        }
+
+        walk_p2(dir, free_size, required);
+    }
+}
+
 fn p1(input: &str) -> u64 {
-    let fs = FsEntry::from_lines(&input.lines().collect::<Vec<_>>()[..], "root".to_owned());
-    0
+    let fs = FsEntry::from_lines(&mut input.lines());
+    if fs.kids.is_some() {
+        let mut total = 0;
+        walk_p1(&fs, &mut total);
+        total as u64
+    } else if fs.size < 100_000 {
+        fs.size as u64
+    } else {
+        panic!("This case was not expected.")
+    }
+}
+
+fn walk_p1(fs: &FsEntry, total: &mut usize) {
+    assert_eq!(fs.size, 0);
+    for dir in fs
+        .kids
+        .as_ref()
+        .unwrap()
+        .iter()
+        .filter(|d| d.kids.is_some())
+    {
+        let dir_total = dir.total_size();
+        if dir_total < 100_000 {
+            *total += dir_total;
+        }
+
+        walk_p1(dir, total);
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -40,167 +98,96 @@ impl FsEntry {
         }
     }
 
-    fn total_size(&self, threshold: usize) -> usize {
-        let mut ret = if self.size < threshold { self.size } else { 0 };
+    fn total_size(&self) -> usize {
+        assert!(self.size > 0 && self.kids.is_none() || self.size == 0);
+        if self.size > 0 {
+            return self.size;
+        }
+
+        let mut ret = 0;
         if let Some(kids) = self.kids.as_deref() {
             for entry in kids {
-                ret += entry.total_size(threshold);
+                ret += entry.total_size();
             }
         }
 
         ret
     }
 
-    fn from_lines_new<I: Iterator<Item = String>>(lines: &mut I) -> FsEntry {
-        return FsEntry::from_lines_new_inner(lines, "/".to_owned());
+    fn from_lines<I: Iterator<Item = impl AsRef<str>>>(lines: &mut I) -> FsEntry {
+        assert_eq!("$ cd /", lines.next().unwrap().as_ref());
+        let mut new = FsEntry::with_name("/".to_owned());
+        new.from_lines_inner(lines);
+        return new;
     }
 
-    fn from_lines_new_inner<I: Iterator<Item = String>>(lines: &mut I, name: String) -> FsEntry {
-        let mut fs = FsEntry::with_name("/".to_owned());
+    fn from_lines_inner<I: Iterator<Item = impl AsRef<str>>>(&mut self, lines: &mut I) {
+        assert_eq!("$ ls", lines.next().unwrap().as_ref());
         loop {
             match lines.next() {
                 Some(line) => {
-                    if line.starts_with("$ ls") {}
-                    if line.starts_with("$ cd") {
-                        let name = &line[4..];
-                        let entry =  FsEntry::from_lines_new_inner(lines, name.to_owned());
-                        if fs.as_ref().kids.is_none() {
-                            fs.kids = Some(vec![entry]);
+                    let s = line.as_ref();
+                    if s.eq("$ cd ..") {
+                        return;
+                    }
+
+                    if s.starts_with("$ cd") {
+                        let name = s.rsplit_once(' ').unwrap().1;
+                        let kid = self
+                            .kids
+                            .as_mut()
+                            .unwrap()
+                            .iter_mut()
+                            .find(|k| k.name.as_str().eq(name))
+                            .unwrap();
+                        kid.from_lines_inner(lines);
+                    } else if s.starts_with("dir ") {
+                        let entry = FsEntry::with_name(s.strip_prefix("dir ").unwrap().to_owned());
+                        if let Some(kids) = self.kids.as_mut() {
+                            kids.push(entry);
                         } else {
-                            fs.kids.unwrap().push(entry);
+                            self.kids = Some(vec![entry]);
+                        }
+                    } else {
+                        let line_bytes = s.as_bytes();
+                        let numbers = line_bytes
+                            .iter()
+                            .take_while(|b| **b < 0x40 && **b >= 0x30)
+                            .count();
+                        let size = unsafe { std::str::from_utf8_unchecked(&line_bytes[..numbers]) }
+                            .parse()
+                            .unwrap();
+                        let name = s.split(' ').skip(1).next().unwrap();
+                        let kid = FsEntry {
+                            name: name.to_owned(),
+                            size,
+                            kids: None,
+                        };
+                        if let Some(kids) = self.kids.as_mut() {
+                            kids.push(kid);
+                        } else {
+                            self.kids = Some(vec![kid]);
                         }
                     }
                 }
                 None => {
-                    return fs;
+                    return;
                 }
             }
         }
     }
+}
 
-    fn from_lines<'a>(mut lines: &'a [&'a str], name: String) -> (&'a [&'a str], Option<FsEntry>) {
-        eprintln!("{}", name);
-        let mut kids = vec![];
-        loop {
-            if lines.len() == 0 {
-                break;
-            }
-
-            if lines[0].starts_with("$ ls") {
-                let mut i = 1;
-                while i < lines.len() && !lines[i].starts_with("$") {
-                    let mut parts = lines[i].split(' ');
-                    match parts.next().unwrap() {
-                        "dir" => {
-                            kids.push(FsEntry::with_name(parts.next().unwrap().to_string()));
-                        }
-                        size => {
-                            let size: usize = size.parse().unwrap();
-                            let name = parts.next().unwrap().to_string();
-                            kids.push(FsEntry {
-                                name,
-                                size,
-                                kids: None,
-                            });
-                        }
-                    }
-                    i += 1;
-                }
-                lines = &lines[i..];
-            } else if lines[0].starts_with("$ cd") {
-                match parse_cd(lines[0]) {
-                    CdAction::Into(s) => {
-                        let res = FsEntry::from_lines(&lines[1..], s);
-                        lines = res.0;
-                        if let Some(entry) = res.1 {
-                            kids.push(entry);
-                        }
-                    }
-                    CdAction::Back => {
-                        lines = &lines[1..];
-                        break;
-                    }
-                    CdAction::Root => break,
-                }
-            } else {
-                panic!("unknown state");
-            }
-        }
-
-        let entry = FsEntry {
-            name,
-            size: 0,
-            kids: if kids.len() == 0 { None } else { Some(kids) },
-        };
-
-        (lines, Some(entry))
+impl AsRef<FsEntry> for FsEntry {
+    fn as_ref(&self) -> &FsEntry {
+        self
     }
 }
 
-fn parse_cd(line: &str) -> CdAction {
-    let mut parts = line.split(' ');
-    let arg = parts.nth(2).unwrap();
-    match arg {
-        ".." => CdAction::Back,
-        "/" => CdAction::Root,
-        a => CdAction::Into(a.to_string()),
+impl AsMut<FsEntry> for FsEntry {
+    fn as_mut(&mut self) -> &mut FsEntry {
+        self
     }
-}
-
-#[derive(Debug, PartialEq)]
-enum CdAction {
-    Back,
-    Root,
-    Into(String),
-}
-
-fn parse_fs_tree(input: &str) -> FsEntry {
-    let mut root = FsEntry {
-        name: "/".to_string(),
-        size: 0,
-        kids: Some(vec![]),
-    };
-
-    let lines = input.lines().collect::<Vec<_>>();
-    let mut remaining = &lines[..];
-    loop {
-        if remaining.is_empty() {
-            break;
-        } else if remaining[0].starts_with("$ ls") {
-            let mut i = 1;
-            while remaining.len() > 0 && !remaining[i].starts_with("$") {
-                let mut parts = remaining[i].split(' ');
-                match parts.next().unwrap() {
-                    "dir" => {}
-                    size => {
-                        let size: usize = size.parse().unwrap();
-                        let name = parts.next().unwrap().to_string();
-                        root.kids.as_mut().unwrap().push(FsEntry {
-                            name,
-                            size,
-                            kids: None,
-                        });
-                    }
-                }
-                i += 1;
-            }
-            remaining = &remaining[i..];
-        } else {
-            match parse_cd(remaining[0]) {
-                CdAction::Root => remaining = &remaining[1..],
-                CdAction::Into(name) => {
-                    let ret = FsEntry::from_lines(&remaining[1..], name);
-                    remaining = ret.0;
-                    if let Some(entry) = ret.1 {
-                        root.kids.as_mut().unwrap().push(entry);
-                    }
-                }
-                CdAction::Back => unreachable!("trying to go back from root"),
-            }
-        }
-    }
-
-    root
 }
 
 #[cfg(test)]
@@ -208,22 +195,96 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parse_cd() {
-        let input = "$ cd /";
-        assert_eq!(CdAction::Root, parse_cd(input));
+    fn should_parse_simple_tree() {
+        let input = vec!["$ cd /", "$ ls", "dir a", "14848514 b.txt", "8504156 c.dat"];
+        let expected = FsEntry {
+            name: "/".to_owned(),
+            size: 0,
+            kids: Some(vec![
+                FsEntry {
+                    name: "a".to_owned(),
+                    size: 0,
+                    kids: None,
+                },
+                FsEntry {
+                    name: "b.txt".to_owned(),
+                    size: 14848514,
+                    kids: None,
+                },
+                FsEntry {
+                    name: "c.dat".to_owned(),
+                    size: 8504156,
+                    kids: None,
+                },
+            ]),
+        };
 
-        let input = "$ cd ..";
-        assert_eq!(CdAction::Back, parse_cd(input));
-
-        let input = "$ cd a";
-        assert_eq!(CdAction::Into("a".to_owned()), parse_cd(input));
-
-        let input = "$ cd ab";
-        assert_eq!(CdAction::Into("ab".to_owned()), parse_cd(input));
+        assert_eq!(expected, FsEntry::from_lines(&mut input.iter()));
     }
 
     #[test]
-    fn p1_test() {
+    fn should_parse_simple_tree_with_subdir() {
+        let input = vec![
+            "$ cd /",
+            "$ ls",
+            "dir a",
+            "14848514 b.txt",
+            "8504156 c.dat",
+            "$ cd a",
+            "$ ls",
+            "dir e",
+            "29116 f",
+            "2557 g",
+            "62596 h.lst",
+        ];
+        let expected = FsEntry {
+            name: "/".to_owned(),
+            size: 0,
+            kids: Some(vec![
+                FsEntry {
+                    name: "a".to_owned(),
+                    size: 0,
+                    kids: Some(vec![
+                        FsEntry {
+                            name: "e".to_owned(),
+                            size: 0,
+                            kids: None,
+                        },
+                        FsEntry {
+                            name: "f".to_owned(),
+                            size: 29116,
+                            kids: None,
+                        },
+                        FsEntry {
+                            name: "g".to_owned(),
+                            size: 2557,
+                            kids: None,
+                        },
+                        FsEntry {
+                            name: "h.lst".to_owned(),
+                            size: 62596,
+                            kids: None,
+                        },
+                    ]),
+                },
+                FsEntry {
+                    name: "b.txt".to_owned(),
+                    size: 14848514,
+                    kids: None,
+                },
+                FsEntry {
+                    name: "c.dat".to_owned(),
+                    size: 8504156,
+                    kids: None,
+                },
+            ]),
+        };
+
+        assert_eq!(expected, FsEntry::from_lines(&mut input.iter()));
+    }
+
+    #[test]
+    fn should_parse_complex_tree() {
         let input = "$ cd /
 $ ls
 dir a
@@ -321,8 +382,65 @@ $ ls
             ]),
         };
 
-        let result = parse_fs_tree(input);
-        dbg!(&expected, &result);
+        let result = FsEntry::from_lines(&mut input.lines());
         assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn should_calc_the_right_answer() {
+        let input = "$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k";
+
+        assert_eq!(95437, p1(input));
+    }
+
+    #[test]
+    fn should_calc_smallest_dir_to_delete() {
+        let input = "$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k";
+
+        assert_eq!(24933642, p2(input));
     }
 }
