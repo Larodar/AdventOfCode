1bd7526fbda9ddb7063f222d936579c4a404c06c
v1p3rh0rr0r <v1p3rh0rr0r@protonmail.com>
2023-12-03 05:23:09 +0100

    more mercury experiments

--- aoc2023/day2/mercury/main.m
+++ aoc2023/day2/mercury/main.m
@@ -2,25 +2,34 @@
 :- interface.
 :- import_module io.
 
-:- pred main(io::di, IO::uo) is cc_multi.
+:- pred main(io::di, io::uo) is cc_multi.
 :- implementation.
-:- import_module char, list, string.
+:- import_module char, list, string, bool, integer, int.
+
+:- type data
+    --->    dist(
+                red :: int,
+                green :: int,
+                blue :: int)
+    ;       info(
+                color :: string,
+                count :: int).
 
 main(!IO) :-
-    solve(!IO, 0, 0, R1, R2),
-    io.format("P1: %d; P2: %d", R1, R2).
+    solve(0, 0, R1, R2, !IO),
+    io.format("P1: %d; P2: %d", [to_string(R1), to_string(R2)], !IO).
     
 
-:- pred solve(!IO, int::in, int::in, int::out, int::out) is semidet.
-solve(!IO, IdTotal, PowerTotal, R1, R2) :-
-    io.read_line_as_string(Resut, !IO),
+:- pred solve(int::in, int::in, int::out, int::out, io::di, io::uo) is det.
+solve(IdTotal, PowerTotal, R1, R2, !IO) :-
+    io.read_line_as_string(Result, !IO),
     (
         Result = eof
     ;
         Result = ok(Line),
         ( process_game(Line, Id, Power),
-          add(IdTotal,Id,IdTotal),
-          add(PowerTotal,Power,PowerTotal),
+          add(IdTotal, Id, IdTotal),
+          add(PowerTotal, Power, PowerTotal)
         ),
         solve(!IO, IdTotal, PowerTotal, R1, R2)
     ;
@@ -32,8 +41,8 @@ solve(!IO, IdTotal, PowerTotal, R1, R2) :-
 :- pred process_game(string::in, int::out, int::out) is det.
 process_game(Line, Id, Power) :-
     (
-        det_head_tail(split_at_char(':',Line), GameInfo, Rest),
-        map(trim_space_prefix, split_at_char(should_split, Rest), CubeInfo),
+        det_head_tail(split_at_char(':', Line), GameInfo, Rest),
+        map(trim_space_prefix, split_at_separator(should_split, Rest), CubeInfo)
     ).
 
 :- pred trim_space_prefix(string::in, string::out) is det.
@@ -42,16 +51,19 @@ trim_space_prefix(Input, Output) :-
         det_remove_prefix(" ", Input, Output)
     ).
 
-:- func cube_info(string) = (string, int).
-cube_info(S) = split_at_char(' '
+:- func cube_info(string::in) = data.
+cube_info(S) = info(Color, to_int(Count)) :-
+    (
+        det_head_tail(split_at_char(' ', S), Count, Tail),
+        det_head(Tail, Color)
+    ).
 
-:- func should_split(char) = bool.
-should_split(';') = true.
-should_split(',') = true.
-should_split(_) = false.
+:- pred should_split(char::in) is semidet.
+should_split(';').
+should_split(',').
 
-:- func update(string, int, int,int,int) = (int, int, int).
-update("red", val, Red, Green, Blue) = (Red `max` val, Green, Blue).
-update("green", val, Red, Green, Blue) = (Red, Green `max` val, Blue).
-update("blue", val, Red, Green, Blue) = (Red, Green, Blue `max` val).
-update(_,_,_,_,_) = (0,0,0).
+:- func update(string, int, data) = data.
+update("red", Val, C) = dist(C^red `max` Val, C^green, C^blue).
+update("green", Val, C) = dist(C^red, C^green `max` Val, C^blue).
+update("blue", Val, C) = dist(C^red, C^green, C^blue `max` Val).
+update(_,_,_) = dist(0,0,0).
