26c42b3ec8a6f6150325a97fe3a971506570bde1
v1p3rh0rr0r <v1p3rh0rr0r@protonmail.com>
2024-12-10 23:30:35 +0100

    2024 day5 zig p1

--- aoc2024/day5/zig/src/main.zig
+++ aoc2024/day5/zig/src/main.zig
@@ -15,28 +15,211 @@ pub fn main() !void {
     if (problem == 1) {
         total = try p1(&stdin);
     } else if (problem == 2) {
-        //total = try p2(&stdin);
+        total = try p2(&stdin);
     }
 
     std.debug.print("result: {?}\n", .{total});
 }
 
 fn p1(reader: anytype) !usize {
+    const allocator = std.heap.page_allocator;
     var buf = [_]u8{0} ** 4096;
     var total: usize = 0;
+    // make this two list of u8?
+    var rules = std.ArrayList(Rule).init(allocator);
+    defer rules.deinit();
     while (try reader.readUntilDelimiterOrEof(buf[0..], '\n')) |l| {
-        if (l.len == 0) {break;}
+        if (l.len == 0) {
+            break;
+        }
+        const idx = std.mem.indexOfScalar(u8, l, '|').?;
+        const lhs = try std.fmt.parseInt(u8, l[0..idx], 10);
+        const rhs = try std.fmt.parseInt(u8, l[idx + 1 ..], 10);
+        try rules.append(Rule{ .first = lhs, .second = rhs });
     }
 
+    var update = std.ArrayList(u8).init(allocator);
+    defer update.deinit();
     while (try reader.readUntilDelimiterOrEof(buf[0..], '\n')) |l| {
+        update.clearRetainingCapacity();
+        var entry_iter = std.mem.splitScalar(u8, l, ',');
+        while (entry_iter.next()) |e| {
+            const entry = try std.fmt.parseInt(u8, e, 10);
+            try update.append(entry);
+        }
+
+        if (check(rules.items[0..], update.items[0..])) {
+            const mid = try std.math.divFloor(usize, update.items.len, 2);
+            total += @as(usize, update.items[mid]);
+        }
     }
-    
+
     return total;
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn p2(reader: anytype) !usize {
+    const allocator = std.heap.page_allocator;
+    var buf = [_]u8{0} ** 4096;
+    var total: usize = 0;
+    // make this two list of u8?
+    var rules = std.ArrayList(Rule).init(allocator);
+    defer rules.deinit();
+    while (try reader.readUntilDelimiterOrEof(buf[0..], '\n')) |l| {
+        if (l.len == 0) {
+            break;
+        }
+        const idx = std.mem.indexOfScalar(u8, l, '|').?;
+        const lhs = try std.fmt.parseInt(u8, l[0..idx], 10);
+        const rhs = try std.fmt.parseInt(u8, l[idx + 1 ..], 10);
+        try rules.append(Rule{ .first = lhs, .second = rhs });
+    }
+
+    var update = std.ArrayList(u8).init(allocator);
+    defer update.deinit();
+    while (try reader.readUntilDelimiterOrEof(buf[0..], '\n')) |l| {
+        update.clearRetainingCapacity();
+        var entry_iter = std.mem.splitScalar(u8, l, ',');
+        while (entry_iter.next()) |e| {
+            const entry = try std.fmt.parseInt(u8, e, 10);
+            try update.append(entry);
+        }
+
+        const u = update.items[0..];
+        std.debug.print("-----------------\n", .{});
+        if (ord(rules.items[0..], u)) {
+            if (!check(rules.items[0..], u)) {
+                std.debug.print("{s}\n", .{l});
+                std.debug.print("-----------------\n", .{});
+            }
+            const mid = try std.math.divFloor(usize, update.items.len, 2);
+            total += @as(usize, update.items[mid]);
+        }
+    }
+
+    return total;
+}
+
+fn ord(rules: []const Rule, u: []u8) bool {
+    var ret = false;
+    for (rules) |r| {
+        if (std.mem.indexOfScalar(u8, u, r.first)) |pos| {
+            if (std.mem.indexOfScalar(u8, u[0..pos], r.second)) |found_at| {
+                ret = true;
+                std.debug.print("{d}|{d}; {d} -> {d}\n", .{r.first, r.second, found_at, pos});
+                std.mem.copyForwards(u8, u[found_at..pos], u[found_at + 1 .. pos]);
+                u[pos - 1] = r.first;
+                u[pos] = r.second;
+            }
+        }
+    }
+
+    return ret;
+}
+
+fn check(rules: []const Rule, update: []const u8) bool {
+    for (rules) |r| {
+        if (std.mem.indexOfScalar(u8, update, r.first)) |pos| {
+            if (std.mem.indexOfScalar(u8, update[0..pos], r.second)) |found_at| {
+                _ = found_at;
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+const ApplyRuleIterator = struct {
+    rules: []const Rule,
+    update: []const u8,
+    idx: usize,
+
+    fn new(rules: []const Rule, update: []const u8) ApplyRuleIterator {
+        return ApplyRuleIterator{
+            .rules = rules,
+            .update = update,
+            .idx = 0,
+        };
+    }
+
+    fn next(self: *ApplyRuleIterator) ?u8 {
+        const r = self.rules[self.idx];
+        if (std.mem.indexOf(u8, self.update, r.first)) |pos| {
+            _ = pos;
+        }
+    }
+};
+
+const Rule = packed struct { first: u8, second: u8 };
+
+test "test p1" {
+    const input =
+        \\47|53
+        \\97|13
+        \\97|61
+        \\97|47
+        \\75|29
+        \\61|13
+        \\75|53
+        \\29|13
+        \\97|29
+        \\53|29
+        \\61|53
+        \\97|53
+        \\61|29
+        \\47|13
+        \\75|47
+        \\97|75
+        \\47|61
+        \\75|61
+        \\47|29
+        \\75|13
+        \\53|13
+        \\
+        \\75,47,61,53,29
+        \\97,61,53,29,13
+        \\75,29,13
+        \\75,97,47,61,53
+        \\61,13,29
+        \\97,13,75,29,47
+    ;
+    var stream = std.io.fixedBufferStream(input);
+    const reader = stream.reader();
+    try std.testing.expectEqual(@as(usize, 143), p1(&reader));
+}
+
+test "test p2" {
+    const input =
+        \\47|53
+        \\97|13
+        \\97|61
+        \\97|47
+        \\75|29
+        \\61|13
+        \\75|53
+        \\29|13
+        \\97|29
+        \\53|29
+        \\61|53
+        \\97|53
+        \\61|29
+        \\47|13
+        \\75|47
+        \\97|75
+        \\47|61
+        \\75|61
+        \\47|29
+        \\75|13
+        \\53|13
+        \\
+        \\75,47,61,53,29
+        \\97,61,53,29,13
+        \\75,29,13
+        \\75,97,47,61,53
+        \\61,13,29
+        \\97,13,75,29,47
+    ;
+    var stream = std.io.fixedBufferStream(input);
+    const reader = stream.reader();
+    try std.testing.expectEqual(@as(usize, 123), p2(&reader));
 }
