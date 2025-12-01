b2856f0f3cb5efa7dec39262ce46afd7cc8a9905
v1p3rh0rr0r <v1p3rh0rr0r@protonmail.com>
2025-12-01 08:55:26 +0100

    2025 day1, cleanup

--- zig_utils/utils.zig
+++ zig_utils/utils.zig
@@ -1,9 +1,20 @@
 const std = @import("std");
 const io = std.io;
 
-//pub fn lines(writer: *io.BufferedReader) void {
-//    writer.reader().readUntilDelimiterOrEof(0x)
-//
-//}
+pub fn StdinLinesIterator(comptime T: type, comptime del_type: type) type {
+    return struct {
+        buffer: []const T,
+        reader: *std.io.Reader,
+        delimiter: switch (del_type) {
+            .sequence, .any => []const T,
+            .scalar => T,
+        },
+        const Self = @This();
 
-
+        pub fn next(self: *Self) []const u8 {
+            if (try self.reader.readUntilDelimiterOrEof(self.buffer[0..], self.delimiter)) |line| {
+                return line;
+            }
+        }
+    };
+}
